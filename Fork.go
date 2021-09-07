dad5e9dd6fbf195ad81cb9c64f40bcc162b36300
MaCoHa <74664350+MaCoHa@users.noreply.github.com>
2021-09-07 13:00:44 +0200

    Fork (#2)
    * made a fork Branch
    
    * made fork components
    
    * made fork comments

--- Fork.go
+++ Fork.go
@@ -7,16 +7,59 @@ package The_dining_philosophers
   or free)
 */
 
-var inputFork = make(chan int)
-var outputFork = make(chan int)
-
 type Fork struct {
 	name      int
 	timesUsed int
+	input     chan int
+	output    chan int
+	inUse     bool
+	f         func()
 }
 
-func NewFork(forkNumber int) *Fork {
-	fork := new(Fork)
-	fork.name = forkNumber
+func NewFork() *Fork {
+	var fork *Fork = new(Fork)
+
+	fork.input = make(chan int)
+	fork.output = make(chan int)
 	return fork
+}
+
+func Forkfunc(fork Fork) {
+	var comand int
+
+	for {
+		comand = <-fork.input
+
+		switch comand {
+		case 1:
+			fork.output <- fork.timesUsed
+
+		case 2:
+			if fork.inUse {
+				//Fork is taken
+				fork.output <- -1
+			} else {
+				//Fork is free
+				fork.output <- -2
+			}
+		case 3:
+			switchstate(fork)
+		}
+	}
+
+}
+
+func switchstate(fork Fork) {
+
+	if fork.inUse {
+		fork.inUse = false
+		//Fork is free
+		fork.output <- -2
+	} else {
+		fork.inUse = true
+		fork.timesUsed++
+		//Fork is taken
+		fork.output <- -1
+	}
+
 }
