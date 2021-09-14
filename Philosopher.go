fe7a5ce5fb6d3a89ac77b126a3634458c7c9d350
coha <coha@itu.dk>
2021-09-14 12:14:50 +0200

    reset

---
+++ Philosopher.go
@@ -0,0 +1,9 @@
+//Philosopher states
+var philosopherIsThinking = -1
+var philosopherIsEating = -2
+
+//Philosopher input commands
+var philosopherAskIsEating = 1
+var philosopherAskTimesEaten = 2
+var philosopherSetEating = 3
+var philosopherSetThinking = 4
